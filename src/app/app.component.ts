8050945d00e5f5f4bccb5353aef82fea029ae349
sandgup3 <sandgup3@cisco.com>
2025-04-14 14:24:21 +0530

    added form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [ReactiveFormsModule, CommonModule],
 })
 export class AppComponent {
   title = 'v-19-momentum-starter';
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.myForm.valid) {
+      console.log('Form Submitted!', this.myForm.value);
+    }
+  }
 }
